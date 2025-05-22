5e672ebf1d7c2a66e0280285d5e5caa5a2ab80f2
CleitinAbacat <danielbclemente0@gmail.com>
2025-05-22 01:16:19 -0300

    Add files via upload

---
+++ src/spotifei/SpotiFei.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package spotifei;
+
+import View.LoginCadastro;
+/**
+ *
+ * @author unifdclemente
+ */
+public class SpotiFei {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        LoginCadastro a = new LoginCadastro();
+        a.setVisible(true);
+
+        //Busca spotify = new Busca();
+        //spotify.setVisible(true);
+        // TODO code application logic here
+    }
+    
+}
